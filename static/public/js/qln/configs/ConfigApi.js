80045a668fd78861685dd6199bee7040cf83d3c3
VuThienSon <vts1381997@gmail.com>
2020-06-13 17:49:31 +0700

    cm

--- static/public/js/qln/configs/ConfigApi.js
+++ static/public/js/qln/configs/ConfigApi.js
@@ -3,15 +3,15 @@ const CONFIG_API = {
     COM_HOME: "/qln/app/common/home",
     COM_DASHBOARD: "/qln/app/common/dashboard",
     LOG_OUT: "/qln/app/common/login",
-    // COM_LOGIN: "https://103.74.122.203:8094/login/",
-    // BASE_API: "https://103.74.122.203:8094/api/",
-    // USER_API: "https://103.74.122.203:8094/phanquyen/",
-    // SEVER: "https://103.74.122.203:3000/",
-    // FILE: "https://103.74.122.203:8094/"
-    COM_LOGIN: "https://localhost:8094/login/",
-    BASE_API: "https://localhost:8094/api/",
-    USER_API: "https://localhost:8094/phanquyen/",
-    SEVER: "https://localhost:3000/",
-    FILE: "https://localhost:8094/"
+    COM_LOGIN: "https://103.74.122.203:8094/login/",
+    BASE_API: "https://103.74.122.203:8094/api/",
+    USER_API: "https://103.74.122.203:8094/phanquyen/",
+    SEVER: "https://103.74.122.203:3000/",
+    FILE: "https://103.74.122.203:8094/"
+    // COM_LOGIN: "https://localhost:8094/login/",
+    // BASE_API: "https://localhost:8094/api/",
+    // USER_API: "https://localhost:8094/phanquyen/",
+    // SEVER: "https://localhost:3000/",
+    // FILE: "https://localhost:8094/"
   }
 };
